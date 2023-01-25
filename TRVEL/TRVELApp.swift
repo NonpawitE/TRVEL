688b0661d514496cf9125651fc45712b250eb8ae
Nonpawit Ekburanawat <nonpawit.ek@gmail.com>
2023-01-25 14:36:51 +0700

    Initial Commit

---
+++ TRVEL/TRVELApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRVELApp.swift
+//  TRVEL
+//
+//  Created by Nonpawit Ekburanawat on 25/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TRVELApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
